84bc87433eebcfcb22e18261198341749bf64474
lukmansp <you@example.com>
2020-04-17 14:21:20 +0700

    create list

---
+++ tempData.js
@@ -0,0 +1,78 @@
+export default tempData = [
+    {
+        name: 'plan a trip',
+        color: '#24a6d9',
+        todos: [
+            {
+                title: 'book flight',
+                completed: true
+            },
+            {
+                title: 'pssport check',
+                completed: true
+            },
+            {
+                title: 'reserve hotel',
+                completed: false
+            }
+        ]
+    },
+    {
+        name: 'holiday',
+        color: '#8022d9',
+        todos: [
+            {
+                title: 'sunny',
+                completed: true
+            },
+            {
+                title: 'vacation',
+                completed: false
+            },
+            {
+                title: 'reserve hotel',
+                completed: false
+            }
+        ]
+    },
+    {
+        name: 'holiday2',
+        color: '#8022d9',
+        todos: [
+            {
+                title: 'sunny',
+                completed: true
+            },
+            {
+                title: 'vacation',
+                completed: false
+            },
+            {
+                title: 'reserve hotel',
+                completed: false
+            },
+            {
+                title: 'reserve hotel',
+                completed: false
+            },
+        ]
+    },
+    {
+        name: 'holiday3',
+        color: '#8022d9',
+        todos: [
+            {
+                title: 'sunny',
+                completed: false
+            },
+            {
+                title: 'vacation',
+                completed: false
+            },
+            {
+                title: 'reserve hotel',
+                completed: false
+            }
+        ]
+    }
+]
